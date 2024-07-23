aa7ec68239d619dcc0e5913ac76ee7872cdd71c1
JessyWts <160591921+JessyWts@users.noreply.github.com>
2024-07-23 18:02:10 +0200

    implement dashboard - edit config to show image frome google, github annd firebase - design dashboard - show profile infos - add buttons to create article, return to dashboard, logout

---
+++ app/dashboard/createArticle/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function PageCreateArticle() {
+  return (
+    <div>PageCreateArticle</div>
+  )
+}
--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,11 +1,13 @@
 'use client';
 
+import DashboardNav from "../components/DashboardNav";
 import ProtectedRoute from "../components/protectedRoute";
 
 export default function DashboardLayout({children}: {children: React.ReactNode}) {
     return (
         <ProtectedRoute>
             <section className="max-w-[1200px] mx-auto w-full mt-2 p-2">
+                <DashboardNav />
                 {children}
             </section>
         </ProtectedRoute>
